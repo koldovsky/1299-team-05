b09d677cd7125b455e76e70000f319d35620c918
Yurii.Davydiuk <126550611+YuriiDavydiuk@users.noreply.github.com>
2024-11-22 15:12:44 +0200

    Update tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -107,10 +107,5 @@ class Shark extends Animal {
     }
   }
 
-https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 
-function circleCircumference(circle) {
-  const { radius } = circle;
-  return +(2 * Math.PI * radius).toFixed(6);
-}
 
