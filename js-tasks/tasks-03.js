8078599f73e2b36bc1b3d11c44973514ba41b12d
Yurii.Davydiuk <126550611+YuriiDavydiuk@users.noreply.github.com>
2024-11-22 13:54:04 +0200

    Update tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -1,1 +1,6 @@
+https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 
+function circleCircumference(circle) {
+  const { radius } = circle;
+  return +(2 * Math.PI * radius).toFixed(6);
+};
