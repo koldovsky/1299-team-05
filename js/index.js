9c775e299c669bfddfd2fed1e4ef36d5bc26c063
Наталія Тишик <nataliaschadilo@gmail.com>
2024-11-25 14:47:34 +0200

    Add file JS

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function init() {
+    import('index.want-to-make-a-step-forward.js');
+}
+
+const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
+    loadedPartialsCount++;
+    if (loadedPartialsCount === totalPartials) init();
+});
