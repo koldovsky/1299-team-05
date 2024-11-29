e3493cb39ec3f99b2b1a3301e8c1207334d63d5d
Юра Давидюк <yurii.davydiuk@sniezka.ukrain>
2024-11-29 13:32:18 +0200

    add subscribe-now js code

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ function init() {
   import("./index.faq-render.js");
   import("./index.faq-accordion.js");
   import("./index.contacts.js");
+  import("./index.subscribe-now.js");
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
